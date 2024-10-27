5b3e185e472369123b9f276640ae4ca8e20e278d
Sharina Chavez-Lissjanis <sha.cha016@gmail.com>
2024-10-27 22:07:48 +0100

    Implements git-reply method

--- Guesser.java
+++ Guesser.java
@@ -7,7 +7,7 @@ import java.io.Console;
  * for the Guesser class. It is your
  * task to complete the missing parts.
  */
-public class Guesser{
+public class Guesser {
   private int low;
   private int high;
 
@@ -15,13 +15,8 @@ public class Guesser{
    * Task 1. Write code here for a constructor
    * which takes two int arguments and saves
    * them in the instance variables low and high.
-   *
-   * If you don't remember the syntax for how to write
-   * constructors, revisit the chapter "Classes"
-   * and review the section about constructors.
-   *
    */
-  
+
   // Write the constructor below this line.
   public Guesser(int low, int high) {
     this.low = low;
@@ -33,18 +28,16 @@ public class Guesser{
    * in the method body, you call first the
    * rules() method, next the doGuesses() method.
    */
-  public void start(){
+  public void start() {
     rules();
     doGuesses();
   }
 
-  
-
-  private void rules(){
+  private void rules() {
     System.out.println("Think of a number between " +
-                       low + " and "  + high);
+        low + " and " + high);
     System.out.println("I'm going to ask a few questions in order " +
-                       "to guess the number.");
+        "to guess the number.");
     System.out.println("Please answer T for true, and F for false.\n");
   }
 
@@ -58,31 +51,33 @@ public class Guesser{
    * a valid reply. Return the String that you read from
    * the player.
    */
-  private String getReply(){
-    String reply = null;
-    // Write code here which reads a String from the console.
-    // As long as it is not a valid reply (one of "T" and "F")
-    // write an error message, and read a new reply.
-    // When you have gotten a valid reply, return it.
+  private String getReply() {
+    System.out.println("Please enter 'T' for true and 'F' for false");
+    String reply = System.console().readLine();
+
+    while (!reply.equalsIgnoreCase("T") && !reply.equalsIgnoreCase("F")) {
+      System.out.println("Invalid input. Type only 'T' or 'F'.");
+      reply = System.console().readLine();
+    }
     return reply;
   }
 
-  private void doGuesses(){
-    int i=0; // number of guesses
-    int middle=0;
-    while(low<high){
+  private void doGuesses() {
+    int i = 0; // number of guesses
+    int middle = 0;
+    while (low < high) {
       // Set next guess to the middle between
       // current low and current high
-      middle=low + (high-low)/2;
+      middle = low + (high - low) / 2;
 
       System.out.println("Is the number less than or equal to " +
-                         middle + "?");
+          middle + "?");
       String reply = getReply();
-      if("T".equals(reply)){
+      if ("T".equals(reply)) {
         // The number is less than or equal to middle
         // so we move down high to middle
         high = middle;
-      }else{
+      } else {
         // The number is greater than middle,
         // so we move up low to middle + 1
         low = middle + 1;
@@ -94,11 +89,11 @@ public class Guesser{
     answer(low, i);
   }
 
-  private void answer(int guess, int numberOfGuesses){
+  private void answer(int guess, int numberOfGuesses) {
     System.out.println("You were thinking about " +
-                       guess +
-                       " (took me " + numberOfGuesses +
-                       " guesses)");
+        guess +
+        " (took me " + numberOfGuesses +
+        " guesses)");
   }
 
 }
